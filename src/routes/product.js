864c96903ae05d17c3ece953a7ba8f833ddd60d2
Your Name <jazzysingh227@gmail.com>
2022-02-17 19:04:32 +0530

    update

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,9 +2,12 @@ const express = require('express');
 const routes = express.Router();
 const getProduct = require('../controller/product');
 const readProduct = require('../controller/productRead');
+const updateProduct = require('../controller/productUpdate');
 
 routes.post('/product', getProduct);
 
 routes.get('/product/read', readProduct);
+
+routes.put('/product/update/:productId', updateProduct);
 
 module.exports = routes;
