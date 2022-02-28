61fc82ef1a9ae01ea806393c3f8baf8e2e529c55
Your Name <jazzysingh227@gmail.com>
2022-02-28 18:45:55 +0530

    All filters in one route done

--- src/controller/products/productFilter.ts
+++ src/controller/products/productFilter.ts
@@ -1,19 +1,98 @@
-import Product from '../../models/product'
-import { Request , Response } from 'express'
+import Product from '../../models/product';
+import { query, Request, Response } from 'express';
+import moment from 'moment';
 
+const filterProduct = async (req: Request, res: Response) => {
+  const { sortBy, maxPrice, minPrice, start, end, name, quantity } = <
+    {
+      start: string;
+      end: string;
+      sortBy: string;
+      maxPrice: string;
+      minPrice: string;
+      name: string;
+      quantity: string;
+    }
+  >req.query;
 
-const filterProduct = async(req:Request , res:Response)=>{
-  const {price} = req.query;
- 
+  const { page = '1', limit = '5' } = <{ page: string; limit: string }>(
+    req.query
+  );
+
+  const startDate = moment(start).startOf('day');
+  const endDate = moment(end).endOf('day');
+
+  const filter: Record<any, any> = {};
+
+  if (start && !end) {
+    filter.createdAt = {
+      $gte: startDate.toDate(),
+    };
+  }
+
+  if (!start && end) {
+    filter.createdAt = {
+      $lte: endDate.toDate(),
+    };
+  }
+
+  if (start && end) {
+    filter.createdAt = {
+      $gte: startDate.toDate(),
+      $lte: endDate.toDate(),
+    };
+  }
+
+  if (maxPrice && !minPrice) {
+    filter.price = {
+      $lte: maxPrice,
+    };
+  }
+
+  if (!maxPrice && minPrice) {
+    filter.price = {
+      $gte: minPrice,
+    };
+  }
+
+  if (maxPrice && minPrice) {
+    filter.price = {
+      $lte: maxPrice,
+      $gte: minPrice,
+    };
+  }
+
+  if (name) {
+    filter.name = {
+      $regex: new RegExp(name),
+    };
+  }
+
+  if (sortBy) {
+    filter.sort = { qty: { quantity: -1 } };
+  }
+
   try {
-    const response = await Product.find({price})
-    res.json({response})
-    
+    const query = Product.find(filter);
+    // if condition query.sort();
+
+    // query.sort();
+    // query.limit();
+
+    // const response = await query;
+    let quantity = 1;
+
+    const response = await Product.find(filter)
+      .sort({ sortBy: quantity })
+      .skip((parseInt(page) - 1) * parseInt(limit))
+      .limit(parseInt(limit));
+
+    res.json({ response, currentPage: page });
   } catch (error) {
     return res.status(403).json({
-      error:"Failed to filter"
-    })
+      error: 'Failed to filter',
+    });
   }
-}
+};
 
 export default filterProduct;
--- src/controller/products/productFilter3.ts
+++ src/controller/products/productFilter3.ts
@@ -1,18 +1,31 @@
-import Product from '../../models/product'
-import { Request , Response } from 'express'
+import Product from '../../models/product';
+import { Request, Response } from 'express';
 
-const filterBySetRange = async(req:Request,res:Response)=>{
+const filterBySetRange = async (req: Request, res: Response) => {
+  const { maxPrice, minPrice } = req.query;
 
-  const {maxPrice , minPrice} = req.query
+  let price;
   try {
-    const result = await Product.find({price:{$lt: maxPrice , $gte:minPrice}})
-    res.json({result})
-    
+    const result = await Product.find({
+      price: { $lt: maxPrice, $gte: minPrice },
+    });
+    if (maxPrice && !minPrice) {
+      price = {
+        $lte: maxPrice,
+      };
+    }
+
+    if (!maxPrice && minPrice) {
+      price = {
+        $gte: minPrice,
+      };
+    }
+    res.json({ result });
   } catch (error) {
     return res.status(403).json({
-      error:"Failed to filter"
-    })
+      error: 'Failed to filter',
+    });
   }
-}
+};
 
 export default filterBySetRange;
